bab4cf7ab9205ec9dceb62a239a0743e1cc9d55c
wasichu <iamwasichu@gmail.com>
2018-02-02 19:28:40 -0600

    Bump version number

--- package/constants.tcl
+++ package/constants.tcl
@@ -1,4 +1,4 @@
-package provide rmq 1.3.1
+package provide rmq 1.3.2
 
 namespace eval rmq {
 	# Frame types
@@ -291,7 +291,7 @@ namespace eval rmq {
 
 	# client properties
 	set PRODUCT "tclrmq"
-	set VERSION "1.3.1"
+	set VERSION "1.3.2"
 }
 
 # vim: ts=4:sw=4:sts=4:noet
--- package/encoders.tcl
+++ package/encoders.tcl
@@ -8,7 +8,7 @@
 ##	will pass through a proc contained in this file
 ##
 ##
-package provide rmq 1.3.1
+package provide rmq 1.3.2
 
 namespace eval rmq {
 
