6f98e647403899939c7699ebbafa9bff56ab7e98
Wasichu <iamwasichu@gmail.com>
2018-02-12 13:48:35 -0600

    Fix broken rand_int range

--- package/Connection.tcl
+++ package/Connection.tcl
@@ -15,7 +15,7 @@ namespace eval rmq {
 
 	# return random integer in [start, end]	
 	proc rand_int {start end} {
-		set range [expr {$start - $end + 1}]
+		set range [expr {$end - $start + 1}]
 		return [expr {$start + int(rand() * $range)}]
 	}
 }
